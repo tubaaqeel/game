948af169e0649c62da3f7db2ea96c9222cb302c9
tubaaqeel <152781352+tubaaqeel@users.noreply.github.com>
2023-12-04 00:33:16 +0500

    script.js

--- script.js
+++ script.js
@@ -99,26 +99,6 @@ function checkWinner() {
 	}
 }
 
-
-// Verifying each selections with winning pattern
-function isWinner(win_pattern, selections){
-
-	var match = 0;
-
-	for (var x=0; x<win_pattern.length; x++) {
-		for (var y=0; y<selections.length; y++) {
-			if (win_pattern[x]==selections[y]) {
-				match++;
-			}
-		}
-	}
-
-	if (match==win_pattern.length) return true;
-
-	return false;
-}
-
-
 // Disable all boxes after winning/draw
 function disableAllBoxes() {
 
@@ -128,42 +108,6 @@ function disableAllBoxes() {
 	}
 
 }
-
-
-// Generating a board for new game
-function generateGame(){
-
-	// Reseting all initialized params as user selected new game
-	resetParams();
-
-	// Getting Variables to update global param
-	game_type = Number(document.getElementById('game_type').value);
-
-	// Clearing board for new game
-	document.getElementById('game-board').innerHTML = '';
-
-	// Generating board
-	for (var row = 1; row <= game_type; row++){
-		for (var col = 1; col <= game_type; col++) {
-			var unique_name = 'grid-'+row+'-'+col;
-			var unique_id = row+''+col;
-			var button = document.createElement("input");
-
-			button.setAttribute("value", ' ');
-			button.setAttribute("id", unique_id);
-			button.setAttribute("name", unique_name);
-			button.setAttribute("class", 'grid-box');
-			button.setAttribute("type", 'button');
-			button.setAttribute("onclick", "markCheck(this)");
-			document.getElementById('game-board').appendChild(button);
-		}
-
-		var breakline = document.createElement("br");
-			document.getElementById('game-board').appendChild(breakline);
-	}
-
-}
-
 
 // Selecting check for desired position
 function markCheck(obj){
@@ -182,71 +126,6 @@ function markCheck(obj){
 
 	checkWinner();
 	changeTurn();
-}
-
-
-
-// Getting most nearest winning and lossing pattern
-function getAutoTurnPattern() {
-
-	var pattern = [];
-	pattern = getMostNearestPattern('Y');
-	if (pattern.length <= 0) {
-		pattern = getMostNearestPattern('X');
-		if (pattern.length <= 0) {
-			pattern = DefaultRobotPatterns();
-		}
-	}
-
-	return pattern;
-	
-}
-
-
-// Getting most applicable pattern for any player
-function getMostNearestPattern(turn){
-
-	var matches = 0;
-
-	var selected = selections[turn].sort();
-	var win_patterns = winnerPatterns();
-
-	finished = false;
-	for (var x=0; x < win_patterns.length; x++) {
-		var intersected = intersectionArray(selected, win_patterns[x]);
-
-		if ( intersected.length==(win_patterns[x].length-1) ) { //return win_patterns[x];
-
-			// if any position is found empty then return that pattern; otherwise will check another one from list
-			for (var y=0; y < win_patterns[x].length; y++) {
-				obj = document.getElementById(win_patterns[x][y]);
-				if (obj.value == '' || obj.value == ' ') {
-					// Return pattern if got an empty; otherwise will match others 
-					return win_patterns[x];	
-				}
-			}
-		}
-
-	}
-	return [];
-}
-
-
-// Return intersaction result by comparing 
-// Players' turns and Winning patterns
-function intersectionArray(x, y){
-
-    var response = [];
-    for (var i = 0; i < x.length; i++) {
-        for (var z = 0; z < y.length; z++) {
-            if (x[i] == y[z]) {
-                response.push(x[i]);
-                break;
-            }
-        }
-    }
-    return response;
-
 }
 
 
