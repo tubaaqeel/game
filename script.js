1af11ca7ecb1c39a8ea1203d0b25d05f5892718d
tubaaqeel <152781352+tubaaqeel@users.noreply.github.com>
2023-12-04 21:23:07 +0500

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var turn = 'X';
-var game_type = [];
+var game_type = 3;
 var total_turns = 0;
 var finished = false;
 
@@ -11,6 +11,20 @@ var selections = new Array();
 var scores = new Array(); 
 	scores['X'] = 0;
 	scores['Y'] = 0;
+	
+// Resetting parameters on reseting game
+function resetParams() {
+	turn = 'X';
+	game_type = 3;
+	total_turns = 0;
+	robot = true;
+	finished = false;
+
+	selections['X'] = new Array();
+	selections['Y'] = new Array();
+}
+
+
 
 // Change turn after another
 function changeTurn(){
@@ -45,8 +59,6 @@ function winnerPatterns() {
 						 		[11,21,31,41,51], [12,22,32,42,52], [13,23,33,43,53], [14,24,34,44,54], [15,25,35,45,55],
 						 		[11,22,33,44,55], [15,24,33,42,51]
 						 	];
-
-					
 
 	return wins
 }
@@ -87,6 +99,26 @@ function checkWinner() {
 	}
 }
 
+
+// Verifying each selections with winning pattern
+function isWinner(win_pattern, selections){
+
+	var match = 0;
+
+	for (var x=0; x<win_pattern.length; x++) {
+		for (var y=0; y<selections.length; y++) {
+			if (win_pattern[x]==selections[y]) {
+				match++;
+			}
+		}
+	}
+
+	if (match==win_pattern.length) return true;
+
+	return false;
+}
+
+
 // Disable all boxes after winning/draw
 function disableAllBoxes() {
 
@@ -96,6 +128,45 @@ function disableAllBoxes() {
 	}
 
 }
+
+
+
+// Generating a board for new game
+function generateGame(){
+
+	// Reseting all initialized params as user selected new game
+	resetParams();
+
+	// Getting Variables to update global param
+	game_type = Number(document.getElementById('game_type').value);
+
+
+
+	// Clearing board for new game
+	document.getElementById('game-board').innerHTML = '';
+
+	// Generating board
+	for (var row = 1; row <= game_type; row++){
+		for (var col = 1; col <= game_type; col++) {
+			var unique_name = 'grid-'+row+'-'+col;
+			var unique_id = row+''+col;
+			var button = document.createElement("input");
+
+			button.setAttribute("value", ' ');
+			button.setAttribute("id", unique_id);
+			button.setAttribute("name", unique_name);
+			button.setAttribute("class", 'grid-box');
+			button.setAttribute("type", 'button');
+			button.setAttribute("onclick", "markCheck(this)");
+			document.getElementById('game-board').appendChild(button);
+		}
+
+		var breakline = document.createElement("br");
+			document.getElementById('game-board').appendChild(breakline);
+	}
+
+}
+
 
 // Selecting check for desired position
 function markCheck(obj){
@@ -114,6 +185,74 @@ function markCheck(obj){
 
 	checkWinner();
 	changeTurn();
+
+	// if auto player selected
+	if (robot===true) autoTurn();
+}
+
+
+
+// Getting most nearest winning and lossing pattern
+function getAutoTurnPattern() {
+
+	var pattern = [];
+	pattern = getMostNearestPattern('Y');
+	if (pattern.length <= 0) {
+		pattern = getMostNearestPattern('X');
+		if (pattern.length <= 0) {
+			pattern = DefaultRobotPatterns();
+		}
+	}
+
+	return pattern;
+	
+}
+
+
+// Getting most applicable pattern for any player
+function getMostNearestPattern(turn){
+
+	var matches = 0;
+
+	var selected = selections[turn].sort();
+	var win_patterns = winnerPatterns();
+
+	finished = false;
+	for (var x=0; x < win_patterns.length; x++) {
+		var intersected = intersectionArray(selected, win_patterns[x]);
+
+		if ( intersected.length==(win_patterns[x].length-1) ) { //return win_patterns[x];
+
+			// if any position is found empty then return that pattern; otherwise will check another one from list
+			for (var y=0; y < win_patterns[x].length; y++) {
+				obj = document.getElementById(win_patterns[x][y]);
+				if (obj.value == '' || obj.value == ' ') {
+					// Return pattern if got an empty; otherwise will match others 
+					return win_patterns[x];	
+				}
+			}
+		}
+
+	}
+	return [];
+}
+
+
+// Return intersaction result by comparing 
+// Players' turns and Winning patterns
+function intersectionArray(x, y){
+
+    var response = [];
+    for (var i = 0; i < x.length; i++) {
+        for (var z = 0; z < y.length; z++) {
+            if (x[i] == y[z]) {
+                response.push(x[i]);
+                break;
+            }
+        }
+    }
+    return response;
+
 }
 
 
